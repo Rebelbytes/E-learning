8c9900ba027feb94d7d5ec8d02524c4b44800936
Sakshe <Sakshe>
2023-09-26 11:58:40 +0530

    sakshi -v1.0.0

---
+++ src/learning/Elearning.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package elearning;
+
+
+public class Elearning {
+
+  
+    public static void main(String[] args) {
+        
+        login loginFrame = new login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+        
+    }
+    
+}
